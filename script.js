39d699c411188a0b55b758208c6901789ab24680
dholandac <daniel.holanda.campos@gmail.com>
2024-05-14 15:16:23 -0300

    updates

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+let keyPressed = false;
+
 const keyDown = e => {
     const key = String(e.keyCode);
-    console.log(key);
+    if (!keyPressed) {
+        console.log(`Tecla de unicode ${key} pressionada!`);
+    }
     document.getElementById(key).style.color = "red";
+    keyPressed = true;
 };
 
 const keyUp = e => {
     const key = String(e.keyCode);
-    console.log(key);
+    console.log(`Tecla de unicode ${key} solta!`);
     document.getElementById(key).style.color = "white";
+    keyPressed = false;
 };
 
 document.addEventListener("keydown", keyDown);
